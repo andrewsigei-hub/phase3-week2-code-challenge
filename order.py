2d2a7014d269f65f22a539595bf81cfaa9733bd5
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-12-01 15:37:48 +0300

    fix order class indentation

--- order.py
+++ order.py
@@ -1,49 +1,44 @@
 class Order:
-    all_orders = []  # Class level list that stores all the orders - AKA SSOT
+    all_orders = []
 
-    # INISTIALISATION
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
-        Order.all_orders.append(self)  ## Adds an order to orders list
+        Order.all_orders.append(self)
 
-        @property  ## Customer getter
-        def customer(self):
-            return self._customer
+    @property
+    def customer(self):
+        return self._customer
 
-        @customer.setter
-        def customer(self, value):
-            from customer import Customer  ## Imports customer class from customer.py
+    @customer.setter
+    def customer(self, value):
+        from customer import Customer
 
-            if not isinstance(
-                value, Customer
-            ):  ## Checks if the value is a customer instance
-                raise TypeError("customer must be a customer instance")
-            self.customer = value
+        if not isinstance(value, Customer):
+            raise TypeError("customer must be a Customer instance")
+        self._customer = value  # <-- use private variable
 
-        @property
-        def coffee(self):
-            return self.coffee
+    @property
+    def coffee(self):
+        return self._coffee
 
-        @coffee.setter
-        def coffee(self, value):
-            from coffee import Coffee  ## Imports coffee class from coffee
+    @coffee.setter
+    def coffee(self, value):
+        from coffee import Coffee
 
-            if not isinstance(value, Coffee):
-                raise TypeError("coffee must be a Coffee instance")
-            self.value = value
+        if not isinstance(value, Coffee):
+            raise TypeError("coffee must be a Coffee instance")
+        self._coffee = value
 
-        @property
-        def price(self):
-            return self.price
+    @property
+    def price(self):
+        return self._price
 
-        @price.setter
-        def price(self, value):
-            if not isinstance(
-                (value, float) or isinstance(value, int)
-            ):  ## IF has a decimal or is an integer then allow else raise an error
-                raise TypeError("Price must be a number")
-            if not (1 <= value <= 10):
-                raise ValueError("Price must be between 1 and 10")
-            self._price = float(value)
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (float, int)):
+            raise TypeError("Price must be a number")
+        if not (1 <= value <= 10):
+            raise ValueError("Price must be between 1 and 10")
+        self._price = float(value)
